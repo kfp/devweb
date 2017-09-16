6a15605756ab070a8bf20435725cd5da4f26180c
Elliott Sperlazza <dev@sperlazzasaurus.com>
2017-09-16 10:47:23 -0400

    added unset link

--- src/components/Portfolio.vue
+++ src/components/Portfolio.vue
@@ -2,11 +2,11 @@
   <div class="index">
     <h3>Portfolio</h3>
     <p>A highlight of entries from my <a href="https://github.com/kfp">GitHub</a> profile</p>
-    <p><a href="">This present website</a> - I wrote this original site in PHP but wanted to try Vue.js and actually enjoyed it quite a bit. It's running in a docker container on a server at home with a free <a href="https://letsencrypt.org/">Let's Encrypt</a> cert.</p>
+    <p><a href="https://github.com/kfp/devweb">This present website</a> - I wrote this original site in PHP but wanted to try Vue.js and actually enjoyed it quite a bit. It's running in a docker container on a server at home with a free <a href="https://letsencrypt.org/">Let's Encrypt</a> cert.</p>
     <p><a href="https://github.com/kfp/kubernetes-tech-talk">kubernetes-tech-talk</a> - After taking a class at Red Hat on docker and kubernetes I kubernetes tutorials (from their repo wiki) and adapted it for a tech talk at the local office. Be sure to check out the <a href="https://github.com/kfp/kubernetes-tech-talk/wiki">wiki</a> for the lab.</p>
     <p><a href="https://github.com/kfp/MagicMirror">MagicMirror</a> - these were all the rage on reddit for a bit so I took the bait. Added a <a href="https://github.com/kfp/MagicMirror-pushbullet">node server</a> to hook into PushBullet which let me send arbitrary messages. Fun project.</p>
     <p><a href="https://github.com/FredXCoders/FredXSite">FredXSite</a> - For a couple of years I helped start and run FredXCoders, a local group which paired computer programming professionals with kids to teach them basics of computer science. I created this bootstrap <a href="http://www.fredxcoders.com/learn/">website</a> to fascilitate the events.</p>
-    <p><a href="">isoStream</a> - A couple of years ago I bought the first 12 seasons of the Simposons on DVD off Ebay and watched through all the commentaries. I wanted a way to digitize the collection without losing access to the DVD features so I used VLC's Java bindings to stream the video/audio of ISO rips of the DVDs. This even worked with menu navigation!</p>
+    <p><a href="https://github.com/kfp/isoStream">isoStream</a> - A couple of years ago I bought the first 12 seasons of the Simposons on DVD off Ebay and watched through all the commentaries. I wanted a way to digitize the collection without losing access to the DVD features so I used VLC's Java bindings to stream the video/audio of ISO rips of the DVDs. This even worked with menu navigation!</p>
   </div>
 </template>
 
